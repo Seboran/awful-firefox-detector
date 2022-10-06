3d2643294b8be0fef0877bc1a222b028dbebb986
Nirina Rabeson <nirinarabeson@me.com>
2022-10-07 00:30:47 +0200

    initial commit :rocket:

---
+++ README.md
@@ -0,0 +1,5 @@
+# awful-firefox-detector
+
+Check out if your browser is firefox or not with this one weird trick!
+
+```yarn dev```
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+let tableau = ref([2, 1]);
+
+onMounted(() => tableau.value.sort((a, b) => (a < b ? 0 : 1)));
+</script>
+
+<template>
+  <h1>Utilisé-je Firefox ?</h1>
+  <br />
+  <h2>{{ tableau[0] < tableau[1] ? "Oui" : "Non" }}</h2>
+</template>
